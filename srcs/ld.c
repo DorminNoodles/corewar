0a7e1ec937cf40b3d14926b5bfc55e2e9361db42
Loic CHETY <loic.chety@gmail.com>
2017-07-23 11:40:40 +0200

    work

--- srcs/ld.c
+++ srcs/ld.c
@@ -6,7 +6,7 @@
 /*   By: lchety <lchety@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/17 20:55:40 by lchety            #+#    #+#             */
-/*   Updated: 2017/07/22 11:54:44 by lchety           ###   ########.fr       */
+/*   Updated: 2017/07/23 11:40:36 by lchety           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	ld(t_vm *vm, t_proc *proc)
 {
 	printf(">>>>ENTER LD<<<<\n");
 
-	// t_bag *bag;
-	//
-	// bag = &vm->p_bag[player];
+	proc->reg[proc->op->ar[1]] = proc->op->ar[0];
+
+
 	// bag->reg[op->ar2] = op->ar1;
 	// printf("FUCKING RES DE CETTE PUTE DE FONCTION DE LDreg => %d\n", bag->reg[op->ar2]);
 	// printf("HHHHHHHHHHHHHHHHHHHHHH\n");
