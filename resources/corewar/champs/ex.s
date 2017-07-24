feb15f266fe6c215c661b24b83a27530a012e67c
Mike LAMBERT <mlambert@e2r12p11.42.fr>
2017-07-24 21:15:49 +0200

    instruction st

--- resources/corewar/champs/ex.s
+++ resources/corewar/champs/ex.s
@@ -1,19 +1,19 @@
 .name "zork"
 .comment "just a basic living prog"
 
-		ld	%200,r1
+#		ld	%200,r1
 		ld	%200,r2
 #		ld	%200,r4
 
-		add r1,r2,r3
-		sub	r1,r2,r4
+#		add r1,r2,r3
+#		sub	r1,r2,r4
 #		sti	r3,%15,%0
 
-		or %1,%8,r8
-		xor %1,%7,r7
-		and %5,%5,r5
-		sti	r5,%20,%0
-		st r5,100
+#		or %1,%8,r8
+#		xor %1,%7,r7
+#		and %5,%5,r5
+#		sti	r5,%20,%0
+		st r2,100
 
 #		sti	r2,%400,%0
 #		sti	r3,%410,%0
--- srcs/st.c
+++ srcs/st.c
@@ -6,7 +6,7 @@
 /*   By: mlambert <mlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/24 15:37:51 by mlambert          #+#    #+#             */
-/*   Updated: 2017/07/24 17:48:32 by lchety           ###   ########.fr       */
+/*   Updated: 2017/07/24 21:09:35 by mlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,26 @@
 
 void	st(t_vm *vm, t_proc *proc)
 {
+	printf("ENTER ST\n");
+
+	int	addr;
 
 	if (proc->op->ar[1] == T_REG)
 		proc->op->ar[1] = proc->reg[proc->op->ar[1]];
-
-		// printf("ST MEM %d \n", proc->op->ar[0]);
-		// printf("ST POS %d \n", (proc->op->pos_opcode + proc->op->ar[1]));
-
-		vm->mem[(proc->op->pos_opcode + proc->op->ar[1]) % MEM_SIZE] = proc->op->ar[0];
+	addr = (proc->op->pos_opcode + (proc->op->ar[1] % IDX_MOD)) % MEM_SIZE;
+	vm->mem[addr] = proc->reg[proc->op->ar[0] - 1];
 
 
+	vm->mem[addr] = proc->op->ar[0];
 
+	printf("addraddr  %d\n", vm->mem[addr]);
+	printf("value un Register %d\n", proc->reg[proc->op->ar[0] - 1]);
+
+/*	vm->mem[addr] = proc->reg[proc->op->ar[0]] >> 24;
+	vm->mem[addr + 1] = proc->reg[proc->op->ar[0]] >> 16;
+	vm->mem[addr + 2] = proc->reg[proc->op->ar[0]] >> 8;
+	vm->mem[addr + 3] = proc->reg[proc->op->ar[0]];*/
 }
+
+
+//(PC + (42 % IDX_MOD)
