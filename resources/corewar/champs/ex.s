90f6add14f0b714f2bb5e722553328a16a5065fd
Loic CHETY <loic.chety@gmail.com>
2017-07-25 22:30:00 +0200

    work

--- resources/corewar/champs/ex.s
+++ resources/corewar/champs/ex.s
@@ -2,7 +2,7 @@
 .comment "just a basic living prog"
 
 #		ld	%600,r1
-		ld	%10,r2
+		ld	1,r2
 #		ld	%200,r4
 #		ld	%200,r8
 
@@ -10,12 +10,12 @@
 #		sub	r1,r2,r4
 #		sti	r3,%15,%0
 
-		ldi	r1,10,r3
+#		ldi	r1,10,r3
 #		or %1,%8,r8
 #		xor %1,%7,r7
 #		and %5,%5,r5
 #		sti	r5,%20,%0
-#		st r2,20
+		st r2,520
 #		sti r8,%500,%0
 
 #		sti	r2,%400,%0
