1cec638eb9ed991381fad1353b4ba4c601b05f53
Remi FULOP <rfulop@e2r9p6.42.fr>
2017-07-31 14:21:04 +0200

    correctifs sur le compteur d'octets

--- srcs/asm/main.c
+++ srcs/asm/main.c
@@ -36,18 +36,16 @@ void  op_no_ocp(int i, char *line)
 int analyse_args(int oct, char *line, int i)
 {
   if (*line == 'r')
-    ++oct;
+    oct += 1;
   else if (*line == '%')
   {
-    printf("pourcent oct = %d i = %d\n", oct);
-    if (i == 1 || i == 2 || i == 6 || i == 7 || i == 9 || i == 13)
+    if ((i == 0 || i == 1 || i == 6 || i == 7 || i == 13))
       oct += 4;
     else
       oct += 2;
   }
   else if (*line == ':')
     oct += 2;
-  printf("'%s' - oct  = %d\n", line, oct);
   return (oct);
 }
 
@@ -56,28 +54,29 @@ int  analyse(int oct, char *line, int i)
   int a;
 
   a = 0;
-  printf("%s\n", line);
-  while (line[a])
+  printf("ANALYSE - %s\n", line);
+  while (line[a] && line[a] != '\n')
   {
     while (line[a] == ':')
       ++a;
-    while (line[a] && (ft_isalpha(line[a]) || ft_isdigit(line[a])))
+    while (line[a] && (line[a] == '-' || ft_isalpha(line[a]) || ft_isdigit(line[a])))
       ++a;
-//      printf("line + %d = '%s'\n", a, line + a);
     while (line[a] && (line[a] == ' ' || line[a] == '\t'))
       ++a;
-//      printf("2line + %d = '%s'\n", a, line + a);
+    if (line[a] == ',')
+      ++a;
     oct = analyse_args(oct, line + a, i);
-//    printf("a = %d\n", a);
     if (line[a] == ',')
       a++; // pour la vrigule tmts
     if (line[a] == '%')
       a++;
+    if (line[a] == '#')
+      return (oct);
   }
     return (oct);
 }
 
-void find_op(char *word, char *line)
+int find_op(char *word, char *line)
 {
   int i;
   int tmp;
@@ -85,7 +84,7 @@ void find_op(char *word, char *line)
 
   oct = 1;
   i = 0;
-  printf("%s\n", word);
+  printf("word '%s'\n", word);
   while (op_tab[i].inst)
   {
     if (!ft_strcmp(word, op_tab[i].inst))
@@ -104,19 +103,21 @@ void find_op(char *word, char *line)
     }
     ++i;
   }
-  printf("tmp = %d\n", tmp);
   oct = analyse(oct, line, tmp);
-  printf("%d\n", oct);
+  printf(" ****** NB OCT %d ******\n", oct);
+  return (oct);
 }
 
-void parse(char *line)
+int parse(char *line)
 {
   int   a;
   int   oct;
   char  *word;
+  int   bytes;
 
   a = 0;
   oct = 0;
+  bytes = 0;
   while (!ft_isprint(line[a]))
     ++a;
   word = take_word(line + a);
@@ -127,40 +128,44 @@ void parse(char *line)
   else if (word[ft_strlen(word) - 1] == LABEL_CHAR)
   {
     ;
-//    printf("is label\n");
 //    bullshit pour tester si le compteur d'octets fonctionne
 //    on recuperera les label et leur postition soon
       while(ft_isalpha(line[a]) || ft_isdigit(line[a]))
         ++a;
-      while (line[a] == ' ' || line[a] == ':')
+      while (line[a] == ' ' || line[a] == '\t' || line[a] == ':')
         ++a;
-      printf("before find op = '%s'\n", line + a);
-      if (line + a) // si, ex: l2: '\n'
-        find_op(word, line + a);
+      free(word);
+      printf("line + a = '%s'\n", line + a);
+      word = take_word(line + a);
+      if (*word)
+        bytes += find_op(word, line + a);
   }
   else
   {
 //    printf("is op\n");
-    find_op(word, line + a);
+    bytes += find_op(word, line + a);
   }
+  return (bytes); // JE TYPE EN RETURN INT POUR RECUPERER dans le main LE nb octect
+  // bisous bisous
 }
 
 int main (int argc, char **argv)
 {
   int   fd;
   char  *line;
+  int   bytes;
 
   line = NULL;
+  bytes = 0;
   fd = open(argv[1], O_RDONLY);
   while (get_next_line(fd, &line))
   {
     if (ft_strlen(line))
     {
-        parse(line);
-        free(line);
+        bytes += parse(line);
+        //  free(line); Ca double free ici, je ne sais pas pourquoi - peut etre mon gnl
     }
-//      printf("%s\n", line);
-//    if (ft_strlen(line))
   }
+  printf("Final bytes number = %d\n", bytes);
   return (0);
 }
---
+++ srcs/asm/te.s
@@ -0,0 +1,8 @@
+.name "test"
+.comment "test test"
+
+ld %1, r2
+ldi %2, r2, r4
+live: zjmp %:l1
+l1:
+l2:
