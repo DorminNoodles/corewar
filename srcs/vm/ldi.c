3aff0a9d1cf3ac6a47260a17fbd5d06b60332cfa
Dormin <loic.chety@gmail.com>
2017-11-23 22:04:08 +0100

    ldi display fix

--- srcs/vm/ldi.c
+++ srcs/vm/ldi.c
@@ -6,7 +6,7 @@
 /*   By: mlambert <mlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/28 00:20:16 by mlambert          #+#    #+#             */
-/*   Updated: 2017/11/23 19:54:45 by lchety           ###   ########.fr       */
+/*   Updated: 2017/11/23 22:01:09 by lchety           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void		ldi(t_vm *vm, t_proc *proc)
 	unsigned int		reg_nb;
 	int ar1;
 	int ar2;
-	int value;
+	unsigned int value;
 
 	value = 0x0;
 	addr = 0;
@@ -58,7 +58,7 @@ void		ldi(t_vm *vm, t_proc *proc)
 		proc->op->ar_typ[1] = DIR_CODE;
 	}
 
-	ft_printf("addr %d\n", proc->op->ar[0] + proc->op->ar[1]);
+	// ft_printf("addr %d\n", proc->op->ar[0] + proc->op->ar[1]);
 
 	addr = (proc->op->ar[0] + proc->op->ar[1]) % IDX_MOD;
 	// ft_printf("addr > %d\n", addr);
@@ -66,15 +66,15 @@ void		ldi(t_vm *vm, t_proc *proc)
 
 	value |= (unsigned char)vm->ram[modulo(addr, MEM_SIZE)].mem;
 	value = value << 8;
-	ft_printf(">>> %d\n", (unsigned char)vm->ram[modulo(addr, MEM_SIZE)].mem);
+	// ft_printf(">>> %x\n", (unsigned char)vm->ram[modulo(addr, MEM_SIZE)].mem);
 	value |= (unsigned char)vm->ram[modulo(addr + 1, MEM_SIZE)].mem;
 	value = value << 8;
-	ft_printf(">>> %d\n", (unsigned char)vm->ram[modulo(addr + 1, MEM_SIZE)].mem);
+	// ft_printf(">>> %x\n", (unsigned char)vm->ram[modulo(addr + 1, MEM_SIZE)].mem);
 	value |= (unsigned char)vm->ram[modulo(addr + 2, MEM_SIZE)].mem;
 	value = value << 8;
-	ft_printf(">>> %d\n", (unsigned char)vm->ram[modulo(addr + 2, MEM_SIZE)].mem);
+	// ft_printf(">>> %x\n", (unsigned char)vm->ram[modulo(addr + 2, MEM_SIZE)].mem);
 	value |= (unsigned char)vm->ram[modulo(addr + 3, MEM_SIZE)].mem;
-	ft_printf(">>> %d\n", (unsigned char)vm->ram[modulo(addr + 3, MEM_SIZE)].mem);
+	// ft_printf(">>> %x\n", (unsigned char)vm->ram[modulo(addr + 3, MEM_SIZE)].mem);
 
 	if (proc->op->ar_typ[2] == REG_CODE && !check_reg(proc->op->ar[2]))
 		return ;
@@ -83,7 +83,7 @@ void		ldi(t_vm *vm, t_proc *proc)
 	if (0x4 & vm->verbosity)
 	{
 		show_operations(vm, proc);
-		ft_printf("\n| -> load from %d + %d = %d (with pc and mod %d)", proc->op->ar[0], proc->op->ar[1], value, addr);
+		ft_printf("\n| -> load from %d + %d = %d (with pc and mod %d)", proc->op->ar[0], proc->op->ar[1], proc->op->ar[0] + proc->op->ar[1], addr);
 		ft_printf("\n");
 	}
 }
