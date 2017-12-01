07750d2c563ec840849153046a474a6402304971
Anna MACIEJEWICZ <amacieje@e2r12p9.42.fr>
2017-12-01 15:19:19 +0100

    ld.c with norm

--- srcs/vm/ld.c
+++ srcs/vm/ld.c
@@ -6,33 +6,41 @@
 /*   By: lchety <lchety@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/17 20:55:40 by lchety            #+#    #+#             */
-/*   Updated: 2017/12/01 14:44:28 by amacieje         ###   ########.fr       */
+/*   Updated: 2017/12/01 15:13:32 by amacieje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void	ld(t_vm *vm, t_proc *proc)
+void		proc_reg(t_vm *vm, t_proc *proc, int reg_nb)
 {
-	int		reg_nb;
 	int		addr;
 
 	addr = 0;
+	addr = proc->op.ar[0] % IDX_MOD;
+	proc->reg[reg_nb] = 0;
+	proc->reg[reg_nb] = (unsigned char)vm->ram[modulo(proc->op.pos_opcode
+		+ addr, MEM_SIZE)].mem;
+	proc->reg[reg_nb] <<= 8;
+	proc->reg[reg_nb] |= (unsigned char)vm->ram[modulo(proc->op.pos_opcode
+		+ addr + 1, MEM_SIZE)].mem;
+	proc->reg[reg_nb] <<= 8;
+	proc->reg[reg_nb] |= (unsigned char)vm->ram[modulo(proc->op.pos_opcode
+		+ addr + 2, MEM_SIZE)].mem;
+	proc->reg[reg_nb] <<= 8;
+	proc->reg[reg_nb] |= (unsigned char)vm->ram[modulo(proc->op.pos_opcode
+		+ addr + 3, MEM_SIZE)].mem;
+}
+
+void		ld(t_vm *vm, t_proc *proc)
+{
+	int		reg_nb;
+
 	reg_nb = proc->op.ar[1];
 	if (!check_params(&proc->op))
 		return ;
 	if (proc->op.ar_typ[0] == IND_CODE)
-	{
-		addr = proc->op.ar[0] % IDX_MOD;
-		proc->reg[reg_nb] = 0;
-		proc->reg[reg_nb] = (unsigned char)vm->ram[modulo(proc->op.pos_opcode + addr, MEM_SIZE)].mem;
-		proc->reg[reg_nb] <<= 8;
-		proc->reg[reg_nb] |= (unsigned char)vm->ram[modulo(proc->op.pos_opcode + addr + 1, MEM_SIZE)].mem;
-		proc->reg[reg_nb] <<= 8;
-		proc->reg[reg_nb] |= (unsigned char)vm->ram[modulo(proc->op.pos_opcode + addr + 2, MEM_SIZE)].mem;
-		proc->reg[reg_nb] <<= 8;
-		proc->reg[reg_nb] |= (unsigned char)vm->ram[modulo(proc->op.pos_opcode + addr + 3, MEM_SIZE)].mem;
-	}
+		proc_reg(vm, proc, reg_nb);
 	else
 		proc->reg[reg_nb] = proc->op.ar[0];
 	proc->carry = (proc->reg[reg_nb] == 0) ? 1 : 0;
