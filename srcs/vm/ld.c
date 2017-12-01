3c69ade5207ff41d6a3ddab68417ae870d07a933
Anna MACIEJEWICZ <amacieje@e2r12p9.42.fr>
2017-12-01 14:47:24 +0100

    42 norm

--- srcs/vm/ld.c
+++ srcs/vm/ld.c
@@ -6,7 +6,7 @@
 /*   By: lchety <lchety@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/17 20:55:40 by lchety            #+#    #+#             */
-/*   Updated: 2017/11/30 17:45:39 by rfulop           ###   ########.fr       */
+/*   Updated: 2017/12/01 14:44:28 by amacieje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,37 +19,27 @@ void	ld(t_vm *vm, t_proc *proc)
 
 	addr = 0;
 	reg_nb = proc->op.ar[1];
-
 	if (!check_params(&proc->op))
 		return ;
 	if (proc->op.ar_typ[0] == IND_CODE)
 	{
 		addr = proc->op.ar[0] % IDX_MOD;
-
 		proc->reg[reg_nb] = 0;
 		proc->reg[reg_nb] = (unsigned char)vm->ram[modulo(proc->op.pos_opcode + addr, MEM_SIZE)].mem;
 		proc->reg[reg_nb] <<= 8;
 		proc->reg[reg_nb] |= (unsigned char)vm->ram[modulo(proc->op.pos_opcode + addr + 1, MEM_SIZE)].mem;
-
-
 		proc->reg[reg_nb] <<= 8;
 		proc->reg[reg_nb] |= (unsigned char)vm->ram[modulo(proc->op.pos_opcode + addr + 2, MEM_SIZE)].mem;
-
-
 		proc->reg[reg_nb] <<= 8;
 		proc->reg[reg_nb] |= (unsigned char)vm->ram[modulo(proc->op.pos_opcode + addr + 3, MEM_SIZE)].mem;
 	}
 	else
 		proc->reg[reg_nb] = proc->op.ar[0];
 	proc->carry = (proc->reg[reg_nb] == 0) ? 1 : 0;
-
 	proc->op.ar[0] = proc->reg[reg_nb];
 	if (0x4 & vm->verbosity)
 	{
 		show_operations(proc);
 		ft_printf("\n");
 	}
-
-	// printf ("proc->reg[1] : %d\n", proc->reg[1]);
-
 }
