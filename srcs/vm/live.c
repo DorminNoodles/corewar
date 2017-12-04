6af8eb0468c002643f095dc2789ac42ab00e1eb8
rfulop <rfulop@student.42.fr>
2017-12-04 14:58:41 +0100

    vm -norme live

--- srcs/vm/live.c
+++ srcs/vm/live.c
@@ -6,7 +6,7 @@
 /*   By: lchety <lchety@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/18 14:03:11 by lchety            #+#    #+#             */
-/*   Updated: 2017/11/30 17:47:33 by rfulop           ###   ########.fr       */
+/*   Updated: 2017/12/04 14:58:26 by rfulop           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,12 @@
 
 void	live(t_vm *vm, t_proc *proc)
 {
-	// printf(">>>>>ENTER_LIVE<<<<<\n");
 	int		num;
+
 	vm->lives_in_cycle++;
 	proc->last_live = vm->cycle + 1;
-
 	num = 0;
-
 	num = proc->op.ar[0] * -1;
-	// printf("num = %d nb player = %d\n", num, vm->nb_player);
-
 	if (4 & vm->verbosity)
 	{
 		show_operations(proc);
@@ -35,6 +31,9 @@ void	live(t_vm *vm, t_proc *proc)
 		vm->player[num].last_live = vm->cycle;
 		vm->ram[modulo(proc->op.pos_opcode, MEM_SIZE)].live = BLING_LIVE;
 		if (5 & vm->verbosity)
-			ft_printf("Player %d (%s) is said to be alive\n", num, vm->player[num].name);
+		{
+			ft_printf("Player %d (%s) is said to be alive\n",
+			num, vm->player[num].name);
+		}
 	}
 }
